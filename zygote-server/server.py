6819595f5762f2dd4bbf73d46fa41904670220a9
deepakkarki <deepak6k@gmail.com>
2014-11-07 20:49:39 +0530

    added a function to convert ImmutableMultiDict of request.form to dict. also wrote some logic under http put handler for gpio (still incomplete)

--- zygote-server/server.py
+++ zygote-server/server.py
@@ -1,16 +1,41 @@
 from flask import Flask, request
 from flask.ext import restful
+import setup as board
 
 app = Flask(__name__)
 api = restful.Api(app)
 
-#TODO : read the config file
-# import the file pointed by config, and use it.
+def get_dict(immu_dict):
+	'''
+	changes the immutable dict to dict
+	'''
+	data = dict(immu_dict)
+	#ImmutableMultiDict([('hello2', u'world2'), ('hello', u'world1'), 
+	#	('hello', u'another-world')])
+	#data : {'hello2': [u'world2'], 'hello': [u'world1', u'another-world']}
+
+	for key in data:
+		val = data[key]
+		if len(val) == 1:
+		#extract from list, covert unicode to str
+			data[key] = str(val[0])
+		else:
+			data[key] = map(str, val)
+	return data
 
 class GPIO(restful.Resource):
-
+	
+	#to configure a gpio pin
 	def put(self, pin):
-		#configure the pin
+		pin_val = board.board_config['GPIO'].get(pin, None)
+
+		#invalid resource point
+		if not pin_val:
+			return "Non existing resource", 404
+
+		args = dict(request.form)
+		#if the pin exists configure it
+		board.config_gpio(pin_val, args)
 		pass
 
 	def get(self, pin):
