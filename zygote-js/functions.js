4ebdfac75abbab9bfec2ad91dc8b3b41ed480bcd
deepakkarki <deepak6k@gmail.com>
2014-12-06 21:09:07 +0530

    clean and upgraded api

--- zygote-js/functions.js
+++ zygote-js/functions.js
@@ -2,258 +2,287 @@
 //The users call these functions in their webpage and the functions will access the appropriate resources through the Flask server.
 //IMPORTANT: This module uses jQuery so include that in your scripts before using this.
 
-zygote = {}
-//For testing purposes, use testServer.php it just prints the URL on the console.
-zygote.server = "testServer.php"
+//TODO : In the functions, sanity check if the user enters the right parameters, before sending an ajax.
 
-//Uncomment this when required.
-//zygote.server = "server.py"
+zygote = {}
+
+//this gives us the sub directory under which the service is available 
+zygote.server = "/board/"
+//eg.	GET	host:port/board/gpio/GPIO1_22
+
 
 //---------------GPIO---------------
-zygote.GPIOconfig = function (pinName, mode, params, callback) {
+zygote.gpio = {
 
-	var configPin = "/gpio/" + pinName;
+	resource : "gpio", //name of the resource
 	
-	params = params || {};
-	params['mode'] = mode;
-	var paramData = $.param(params);
+	url : zygote.server + "gpio" + "/", //base url for gpio
 	
-	callback = callback || function() {};
-	
-	$.ajax({
-		url : server + configPin,
-		type : "PUT",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
-	
-}
+	config : function (pinName, mode, params, callback) {
 
-zygote.GPIOread = function (pinName, params, callback) {
+		var configPin =  this.url + pinName;
+	
+		params = params || {};
+		params['mode'] = mode;
+		var paramData = $.param(params);
+	
+		callback = callback || function() {};
+	
+		$.ajax({
+			url : configPin,
+			type : "PUT",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
+	
+	},
 
-	var readPin = "/gpio/" + pinName;
-	
-	params = params || {};
-	var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
-	
-	callback = callback || function() {};
-	 
-	$.ajax({
-		url : server + readPin,
-		type : "GET",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
-	
-}
+	read : function (pinName, params, callback) {
 
-zygote.GPIOwrite = function (pinName, status, params, callback) {
+		var readPin =  this.url + pinName;
+	
+		params = params || {};
+		var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
+	
+		callback = callback || function() {};
+		 
+		$.ajax({
+			url : readPin,
+			type : "GET",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
+	
+	},
 
-	var writePin = "/gpio/" + pinName;
+	write : function (pinName, status, params, callback) {
+
+		var writePin =  this.url + pinName;
 	
-	params = params || {};
-	params['status'] = status;
-	var paramData = $.param(params);
+		params = params || {};
+		params['status'] = status;
+		var paramData = $.param(params);
 	
-	callback = callback || function() {};
-	 
-	$.ajax({
-		url : server + writePin,
-		type : "POST",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
+		callback = callback || function() {};
+		 
+		$.ajax({
+			url : writePin,
+			type : "POST",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
+	}
 }
 
 //---------------PWM---------------
-zygote.PWMconfig = function (pinName, enable, params, callback) {
+zygote.pwm = {
 
-	var configPin = "/pwm/" + pinName;
+	resource : "pwm", //name of the resource	
+	url : zygote.server + "pwm" + "/", //base url for pwm 
 	
-	params = params || {};
-	params['status'] = status;
-	var paramData = $.param(params);
+	config : function (pinName, enable, params, callback) {
+
+		var configPin = this.url + pinName;
 	
-	callback = callback || function() {};
-	 
-	$.ajax({
-		url : server + configPin,
-		type : "PUT",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message)
-		},
-		complete : callback
-	});
+		params = params || {};
+		params['status'] = status;
+		var paramData = $.param(params);
 	
+		callback = callback || function() {};
+		 
+		$.ajax({
+			url : configPin,
+			type : "PUT",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message)
+			},
+			complete : callback
+		});
+	
+	},
+
+	write : function (pinName, value, params, callback) {
+
+		var writePin = this.url + pinName;
+	
+		params = params || {};
+		params['value'] = value;
+		var paramData = $.param(params);
+	
+		callback = callback || function() {};
+		 
+		$.ajax({
+			url : writePin,
+			type : "POST",
+			data: paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message)
+			},
+			complete : callback
+		});
+	}
 }
 
-zygote.PWMwrite = function (pinName, value, params, callback) {
-
-	var writePin = "/pwm/" + pinName;
-	
-	params = params || {};
-	params['value'] = value;
-	var paramData = $.param(params);
-	
-	callback = callback || function() {};
-	 
-	$.ajax({
-		url : server + writePin,
-		type : "POST",
-		data: paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message)
-		},
-		complete : callback
-	});
-}
 
 //---------------SERVO---------------
-zygote.SERVOconfig = function (pinName, enable, params, callback) {
+zygote.servo = {
 
-	var configPin = "/servo/" + pinName;
+	resource : "servo", //name of the resource	
+	url : zygote.server + "servo" + "/", //base url for servo
 	
-	params = params || {};
-	params['enable'] = enable;
-	var paramData = $.param(params);
-	
-	callback = callback || function() {};
-	
-	$.ajax({
-		url : server + configPin,
-		type : "PUT",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
-	
-}
+	config : function (pinName, enable, params, callback) {
 
-zygote.SERVOread = function (pinName, params, callback) {
+		var configPin = this.url + pinName;
+	
+		params = params || {};
+		params['enable'] = enable;
+		var paramData = $.param(params);
+	
+		callback = callback || function() {};
+	
+		$.ajax({
+			url : configPin,
+			type : "PUT",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
+	
+	},
 
-	var readPin = "/servo/" + pinName;
-	
-	params = params || {};
-	var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
-	
-	callback = callback || function() {};
-	 
-	$.ajax({
-		url : server + readPin,
-		type : "GET",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
-	
-}
+	read : function (pinName, params, callback) {
 
-zygote.SERVOwrite = function (pinName, angle, params, callback) {
+		var readPin = this.url+ pinName;
+	
+		params = params || {};
+		var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
+	
+		callback = callback || function() {};
+		 
+		$.ajax({
+			url : readPin,
+			type : "GET",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
+	
+	},
 
-	var writePin = "/servo/" + pinName;
+	write : function (pinName, angle, params, callback) {
+
+		var writePin = this.url + pinName;
 	
-	params = params || {};
-	params['angle'] = angle;
-	var paramData = $.param(params);
+		params = params || {};
+		params['angle'] = angle;
+		var paramData = $.param(params);
 	
-	callback = callback || function() {};
-	 
-	$.ajax({
-		url : server + writePin,
-		type : "POST",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
+		callback = callback || function() {};
+		 
+		$.ajax({
+			url : writePin,
+			type : "POST",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
+	}
 }
 
 //---------------AIN---------------
-function AINconfig(pinName, enable, params, callback) {
+zygote.ain = {
 
-	var configMode = "enable=" + enable;
-	var configPin = "/ain/" + pinName;
+	resource : "ain", //name of the resource	
+	url : zygote.server + "ain" + "/", //base url for analog in
+	
+	config : function (pinName, enable, params, callback) {
+
+		var configMode = "enable=" + enable;
+		var configPin = this.url + pinName;
 	
 	
-	params = (typeof params === "undefined") ? {} : params;
-	var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
+		params = (typeof params === "undefined") ? {} : params;
+		var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
 		
-	callback = (typeof callback === "undefined") ? function() {} : callback;
+		callback = (typeof callback === "undefined") ? function() {} : callback;
 	
-	$.ajax({
-		url : server + configPin,
-		type : "PUT",
-		data : configMode + "&" + paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
+		$.ajax({
+			url : configPin,
+			type : "PUT",
+			data : configMode + "&" + paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
 	
+	},
+
+	read : function (pinName, params, callback) {
+
+		var readPin = this.url + pinName;
+	
+		params = (typeof params === "undefined") ? {} : params;
+		var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
+		
+		callback = (typeof callback === "undefined") ? function() {} : callback;
+		 
+		$.ajax({
+			url :  readPin,
+			type : "GET",
+			data : paramData,
+			success : function (response) {
+				console.log(response);
+			},
+			error : function (error) {
+				console.log(error.message);
+			},
+			complete : callback
+		});
+	
+	}
 }
 
-function AINread(pinName, params, callback) {
-
-	var readPin = "/ain/" + pinName;
-	
-	params = (typeof params === "undefined") ? {} : params;
-	var paramData = ($.isEmptyObject(params)) ? "" : $.param(params);
-		
-	callback = (typeof callback === "undefined") ? function() {} : callback;
-	 
-	$.ajax({
-		url : server + readPin,
-		type : "GET",
-		data : paramData,
-		success : function (response) {
-			console.log(response);
-		},
-		error : function (error) {
-			console.log(error.message);
-		},
-		complete : callback
-	});
-	
-}
