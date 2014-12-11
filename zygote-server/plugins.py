53b9a3b4d33af55a9b5a5c488df6511112209903
deepakkarki <deepak6k@gmail.com>
2014-12-11 20:27:00 +0530

    added new pseudo sensors

--- zygote-server/plugins.py
+++ zygote-server/plugins.py
@@ -11,16 +11,34 @@ plugin_set = ['TEMP', 'LOC']
 ########################
 from random import randint
 
+#show in gauge
 class TEMP(restful.Resource):
     def get(self):
 	temp = randint(20,30)
 	print temp
         return str(temp), 200
 
+#show in MAP
 class LOC(restful.Resource):
 	def get(self):
 		#return "12.934268,77.534326,17z", 200
 		return "37.240,-115.81111", 200
 		#latitude, longitide, zoom level - for PESU
 
-##add magnetometer :p
+#show in pointer
+class COMPASS(restful.Resource):
+	def get(self):
+		return "90"
+
+#show in graph
+class GAS(restful.Resource):
+	def get(self):
+		concentration = randint(20,25)
+        	return str(concentration), 200
+
+#binary light representation
+class BELL(restful.Resource):
+	def get(self):
+		return str(randint(0,1)), 200
+
+
