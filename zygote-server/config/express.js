918bdec91388d285889d74fe0419f808bd2d2dbb
deepakkarki <deepak6k@gmail.com>
2015-02-05 21:02:24 +0530

    Added middleware and routes to make MVC work; now it can handle JSON data

--- zygote-server/config/express.js
+++ zygote-server/config/express.js
@@ -5,6 +5,16 @@ module.exports = function() {
 	//get the actual express module
 	var app = express();
 
+	//other modules 
+	var bodyParser = require('body-parser');
+	var multer = require('multer');
+
+	app.use(bodyParser.text()); // for parsing plain text
+	app.use(bodyParser.json()); // for parsing application/json
+	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+	app.use(multer()); // for parsing multipart/form-data
+
+	
 	//add our routes to it
 	require('../app/routes/routes.js')(app);
 
