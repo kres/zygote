d5512a2d3180efcf92a26ead22333d359fa450ee
deepakkarki <deepak6k@gmail.com>
2015-02-05 23:29:53 +0530

    changed route to only accept url with trailing slashes (i.e.) resource types and not resource instances

--- zygote-server/app/routes/routes.js
+++ zygote-server/app/routes/routes.js
@@ -4,5 +4,5 @@ module.exports = function(app) {
 		var index = require('../controllers/index.js');
 		var container = require('../controllers/container.js');
 		app.get('/', index);
-		app.all('/container/*', container);
+		app.all('/container/*/$', container);
 	};
