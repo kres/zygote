af4095fb160159e74ba931977f5185c427643bf8
deepakkarki <deepak6k@gmail.com>
2015-02-11 01:07:28 +0530

    added custom middleware to convert string into json if client misses content-type header

--- zygote-server/config/express.js
+++ zygote-server/config/express.js
@@ -13,7 +13,7 @@ module.exports = function() {
 	app.use(bodyParser.json()); // for parsing application/json
 	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 	app.use(multer()); // for parsing multipart/form-data
-
+	app.use(jsonify);
 	
 	//add our routes to it
 	require('../app/routes/routes.js')(app);
@@ -21,3 +21,16 @@ module.exports = function() {
 	//return express obj with our settings
 	return app;
 };
+
+function jsonify(req, res, next){
+	if((req.method != 'GET') && (typeof(req.body) == 'string')){
+		try{
+			req.body = JSON.parse(req.body);
+		}
+		catch(e){
+			res.status(400).send({"error": "request body not json format"});
+			return;
+		}
+	}
+	next();
+}
