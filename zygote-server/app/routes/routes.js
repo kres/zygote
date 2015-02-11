aea3112875f86ee68eb8f381cc1b1ea280bec555
deepakkarki <deepak6k@gmail.com>
2015-02-12 00:37:08 +0530

    added ability to fetch the pin and ep data via REST url

--- zygote-server/app/routes/routes.js
+++ zygote-server/app/routes/routes.js
@@ -4,9 +4,14 @@ module.exports = function(app) {
 		var index = require('../controllers/index.js');
 		var res_type = require('../controllers/res_type.js');
 		var containers = require('../controllers/containers.js')
+		var data =  require('../controllers/data.js');
 
 		app.get('/', index);
 
 		app.get('/containers/', containers.get);
 		app.post('/containers/', containers.post);
+
+		app.get('/meta', function(req, res){
+				res.json(data.meta);
+			})
 	};
