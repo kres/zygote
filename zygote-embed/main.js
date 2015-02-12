35d56229629c2ea07e1c88dc9d764899b16a6f7f
deepakkarki <deepak6k@gmail.com>
2015-02-12 22:04:12 +0530

    modifications to the socket io code to enable talk b/w virtual hardware (bbb and rpi)

--- zygote-embed/main.js
+++ zygote-embed/main.js
@@ -1,7 +1,24 @@
 //the main js file handles the communication to and from zygote server
 //powered by socket-io-client
-var url = 'bbb';
+var url = process.argv[2] || 'bbb';
 var socket = require('socket.io-client')('http://localhost:3000');
 socket.on('connect', function(){
-	socket.emit('url', {"data": url});
+	console.log('connected to zygote as url : ' + url);
+	socket.emit('url',  url);
+	socket.on('data', function(data, callback){
+		console.log(data);
+		//here it should actually 
+		callback({"status":"OK"});
+	});
+	//rpi is the mock client h/w for testing purposes
+	if(url == 'rpi'){
+		socket.emit('data', 
+			{"container" : "bbb", 
+			 "res" : "gpio/1",
+			 "op": "read",
+			 "data" : {"foo":"bar"}
+			 }, function(ret_val){
+			 	console.log(ret_val);
+				});
+	}
 });
--- zygote-server/app/controllers/socket_handler.js
+++ zygote-server/app/controllers/socket_handler.js
@@ -2,13 +2,45 @@
 //does routing work too
 var data = require('./data.js');
 
+//contains container name to socket object mapping
+var sock_map = {};
+
 exports.startSocket = function(server){
 	//create the web-socket manager
 	var io = require('socket.io')(server);
 
 	io.on('connection', function(socket){
-		socket.on('url', function(data){
-			console.log(data);
+
+		socket.on('url', function(msg){
+			//msg is a string, such as : 'bbb', 'rpi'
+			//if the board is available 
+			if(msg in data.spec){
+				//add a mapping 
+				sock_map[msg] = socket;
+				
+				//add data handler
+				socket.on('data', function(req, callback){
+					//req contains the request data
+					//req : {'container' : 'bbb', 'res' : 'gpio/1', 'op' : 'read',data : {}}
+					if(req['container'] in sock_map){
+						sock_map[req['container']].emit('data', req, function(ret_val){
+							callback(ret_val);
+						})
+					}
+					else{
+						callback({"error": "no such container"});
+					}
+				});
+
+				//add disconnect handler 
+				socket.on('disconnect', function(){
+					if(msg in sock_map) delete sock_map[msg];
+				});
+
+			}
+			else{
+				socket.disconnect();
+			}
 		});
-	})
+	});
 }
