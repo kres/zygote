36b2277482e43264145a7c608e35471f1bb5d1f6
deepakkarki <deepak6k@gmail.com>
2015-02-15 18:04:32 +0530

    The remote controller now exposes RWC interface and also handles create and delete requests

--- zygote-embed/remote_controller.js
+++ zygote-embed/remote_controller.js
@@ -4,28 +4,69 @@
 //handles events it gets from the socket
 //will r-w-c from loacl_controller and also create-delete res in it
 
+var lc = require("./loacl_controller");
+//gatway is basically the socket interface
+var gateway = null;
+
 exports.start = function start(server){
+
 	var socket = require('socket.io-client')(server);
+
 	socket.on('connect', function(){
 		console.log('connected to zygote as url : ' + url);
-		socket.emit('url',  url);
+		socket.emit('url', {"url" : url});
+		gateway = socket;
 
-		socket.on('data', function(data, callback){
-			console.log(data);
-			//here it should actually do the processing
-			//read-write-config an onboard resource
-			callback({"status":"OK"});
+		socket.on('data', function(rpc_struct, callback){
+			console.log(rpc_struct);
+			var ep = rpc_struct['ep'];
+			if(rpc_struct['op'] == 'read'){
+				lc.read(ep, rpc_struct['data'], callback);
+			}
+			else if(rpc_struct['op'] == 'write'){
+				lc.write(ep, rpc_struct['data'], callback);
+			}
+			else if(rpc_struct['op'] == 'config'){
+				lc.config(ep, rpc_struct['data'], callback);
+			}
+			else{
+				callback({"error":"operation not supported"});
+			}
+
 		});
 
-		socket.on('create', function(data, callback){
+		socket.on('create', function(rpc_struct, callback){
 			//create a resource
+			var ep = rpc_struct['ep'];
+			console.log("Create called");
+			lc.create(ep, rpc_struct['data'], callback);
 		});
 
-		socket.on('delete', function(data, callback){
+		socket.on('delete', function(rpc_struct, callback){
 			//remove a resource
+			var ep = rpc_struct['ep'];
+			console.log("Delete called");
+			lc.delete(ep, rpc_struct['data'], callback);
 		});
 
 	});
 }
 
 //export read-write-config
+export.read = function(rpc_struct, callback){
+	rpc_struct['op'] = 'read';
+	//callback is a fn that takes one param --> viz resulting_data
+	gateway.emit("data", rpc_struct, callback);
+}
+
+export.write = function(rpc_struct, callback){
+	rpc_struct['op'] = 'write';
+	//callback is a fn that takes one param --> viz resulting_data
+	gateway.emit("data", rpc_struct, callback);
+}
+
+export.config = function(rpc_struct, callback){
+	rpc_struct['op'] = 'config';
+	//callback is a fn that takes one param --> viz resulting_data
+	gateway.emit("data", rpc_struct, callback);
+}
