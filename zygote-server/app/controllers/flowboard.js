d96add21411573596b547f5cf20bc4d45cb2d1f9
deepakkarki <deepak6k@gmail.com>
2015-02-16 23:30:05 +0530

    handles incoming scripts

--- zygote-server/app/controllers/flowboard.js
+++ zygote-server/app/controllers/flowboard.js
@@ -1,7 +1,31 @@
 //post new js code to execute
 //format??
+// {'target' : 'bbb', script="<well,the script... duh!>"}
+
+var data = require("./data.js");
+var sc = require("./socket_handler.js");
+
 exports.post = function(req, res){
 	//handles post request
 	console.log("code posted to flowboard");
-	res.json({"status": "ok"});
+
+	var target = req.body['target'];
+	//script is a json {code : "code-as-str"}
+	var script = req.body['script'];
+	//script can be built here; client can send json
+
+	if(!target || !script){
+		res.json({"error": "incorrect data format"});
+		return;
+	}
+
+	if(target in data.spec){
+		sc.execute(target, script, function(data){
+			res.json(data);
+		});
+	}
+
+	else{
+		res.json({"error": "target container not found"});
+	}
 }
