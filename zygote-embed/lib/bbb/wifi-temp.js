738e6deb830b470aae10a0c8e8e6f7c9247f5f9e
deepakkarki <deepak6k@gmail.com>
2015-03-06 22:00:40 +0530

    added support for *real* wifi/network based temperature sensor node

--- zygote-embed/lib/bbb/wifi-temp.js
+++ zygote-embed/lib/bbb/wifi-temp.js
@@ -6,6 +6,20 @@ function init(ep, opts, callback){
 	this.ip = opts['wifi'];
 	console.log("opts: ", JSON.stringify(opts));
 	console.log("Wifi temp created");
+
+	//the wifi temp sensor listens @ port 3003
+	var PORT = 3003;
+
+	var client = new net.Socket();
+	client.connect(PORT, this.ip, function() {
+		console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+		this.sock = client;
+	});
+
+	// Add a 'close' event handler for the client socket
+	client.on('close', function() {
+		console.log('Connection closed');
+	});
 	callback(this);
 }
 
@@ -13,7 +27,11 @@ function init(ep, opts, callback){
 init.prototype.read = function read(data, callback){
 	console.log("WIFI-temp read");
 	console.log("EP : "+this.ep+" IP : "+ this.ip);
-	callback({"value" : "ok"});
+	this.sock.write("read");
+	this.sock.on('data', function(data){
+		console.log("data : ", data);
+		callback({"value" : data});
+	});
 }
 
 init.prototype.write = function write(data, callback){
@@ -32,6 +50,7 @@ init.prototype.config = function config(data, callback){
 init.prototype.delete = function del(data, callback){
 	console.log("WIFI-temp delete");
 	console.log("EP : "+this.ep+" IP : "+ this.ip);
+	this.sock.destroy();
 	callback({"value" : "ok"});
 
 }
