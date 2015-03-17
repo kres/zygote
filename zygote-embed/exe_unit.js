ec0322c64196f5e7b6f90868fddcfaea434bc78d
deepakkarki <deepak6k@gmail.com>
2015-03-17 22:53:34 +0530

    added support for timer based flows

--- zygote-embed/exe_unit.js
+++ zygote-embed/exe_unit.js
@@ -3,18 +3,44 @@
 //hardcode some test scripts 
 var Resource = require("./resource.js");
 var conf = require("./conf");
-conf.flows = {}
 
 exports.execute = function(flow_id, flow_struct){
-	//flow struct is the json that holds info 
-	//about the flow to be executed
+	//flow struct is the json that holds info about the flow to be executed
 
 	console.log('executing script : ', flow_id);
 	console.log(flow_struct);
-	
-	//blindly evaluate the script for now
-	eval(flow_struct['flow']);
 
+	//check if flow already exists
+	if(flow_id in conf.flows){
+		console.log("Flow exists! Delete first. Aborting...");
+		return;
+	}
+
+	//check if trigger is okay
+	if(flow_struct.trigger['type'] == 'timer'){
+		var timer_val = parseInt(flow_struct.trigger['val']);
+
+		var flow_func = eval(flow_struct['flow']);
+		
+		conf.flows[flow_id] = flow_struct;
+		
+		var func_to_execute = function(){
+			//if the flow exists, schedule again
+			//flow_id won't exist if it has been deleted
+			flow_func();
+			conf.flows[flow_id]['trigger']['obj'] = setTimeout(flow_func, timer_val*1000);
+		}
+		conf.flows[flow_id]['code'] = func_to_execute;
+		func_to_execute();
+	}
+
+	else if(flow_struct.trigger['type'] == 'event'){
+		//TODO : implement event based triggers
+	}
+	else{
+		console.log("Invalid trigger type");
+		return;
+	}
 	//flow_struct['flow'] would be something like
 	/*
 	function foobar(){
