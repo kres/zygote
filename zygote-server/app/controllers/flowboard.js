8fdc7edd7392e05526e6c2207c12e7a860d15e43
deepakkarki <deepak6k@gmail.com>
2015-03-21 17:12:27 +0530

    1. Added comment  2. Fixed bug of passing wrong variable to sc.execute

--- zygote-server/app/controllers/flowboard.js
+++ zygote-server/app/controllers/flowboard.js
@@ -11,17 +11,30 @@ exports.post = function(req, res){
 
 	/*
 		flow_set : 
+
 		{
-			"flow-id" : {...} //one and one only
+			"flow-id" : {
+				"flow" : "<code-string>",
+				"target" : "<container-name>",
+				"trigger" : {
+					"type"  : "['timer'|'event']",
+					"val" : "['number'|'url']", //number=> time in sec, url=>'gpio/1'
+					"obj"	:  [<timer-obj> | <event-emmiter-obj>] //filled on client side
+					"event" : "name-of-event" //only for event emitter types
+				},
+				"code" : <function-obj> //generated code (by client), includes trigger
+			}
 		}
+
 	*/
-	var flow_set = req.body['flows'];
+	var flow_set = req.body;
 	
 	for(flow_id in flow_set){
+		console.log("Flow id : " +flow_id);
 		var target = flow_set[flow_id]['target'];
 
 		if(target in data.spec){
-			sc.execute(target, flow_struct, function(data){
+			sc.execute(target, flow_set, function(data){
 				res.json(data);
 			});
 		}
