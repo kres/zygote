afbce472fcdf875e07cb8e250a9f61ab668d08a7
deepakkarki <deepak6k@gmail.com>
2015-03-24 07:58:14 +0530

    more filler to the function

--- zygote-server/app/controllers/dashboard.js
+++ zygote-server/app/controllers/dashboard.js
@@ -4,33 +4,38 @@
 //There could be subtle differences that could lead to a problem. So seprate for now
 
 function read (panel, widget, args, callback) {
-	// body...
+	// uses socket 'ds'
 }
 
 function write (panel, widget, args, callback) {
-	// body...
+	// uses socket 'ds'
 }
 
 function config (panel, widget, args, callback) {
-	// body...
+	// uses socket 'ds'
 }
 
+exports.read = read;
+exports.write = write;
+exports.config = config;
+
+/******************************************************************/
 
 //The following two functions are the result of user creating/deleting widgets on dashboard
 
-function create (panel, widget, args, callback) {
-	// body...
+function create (panel, widget, args) {
+	// manipulate the dashboard_tree
 }
 
-function delete (panel, widget, args, callback) {
-	// body...
+function del (panel, widget) {
+	// manipulate the dashboard_tree
 }
-
-
 
 // following structure is updated by create and delete
 // For internal use only
 // Should expose functions to get the reqd values to display in flowboard
+		//function get_panels()
+		//function get_widgets("panel-id");
 
 var dashboard_tree = {}
 /*
@@ -53,3 +58,36 @@ var dashboard_tree = {}
 	...
 }
 */
+
+function get_panels(){
+	return panels;
+}
+
+function get_widgets (panel_id) {
+	return widgets;
+}
+
+exports.get_panels = get_panels;
+exports.get_widgets = get_widgets;
+
+/*****************************************************************************/
+
+//The main socket handler
+var ds;
+exports.startSocket = function(server){
+	//create the web-socket manager
+	var io = require('socket.io')(server);
+	var dsr = io.of('/dashboard');
+
+	dsr.on("connection", function(socket){
+		console.log("Connected to dashboard!!");
+		ds = socket;
+		socket.on("create", function(data, callback){
+			create(data['panel'], data['widget'], data['widget-info']);
+		});
+
+		socket.on("delete", function(data, callback){
+			del(data['panel'], data['widget']);
+		});
+	});
+}
