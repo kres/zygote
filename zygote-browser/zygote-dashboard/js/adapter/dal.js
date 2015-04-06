05093203c6b88b3e5688193fd80904e08c61aef2
deepakkarki <deepak6k@gmail.com>
2015-04-06 22:40:54 +0530

    added the interface for data abstraction layer. For Dashboard, flows, resources.

--- zygote-browser/zygote-dashboard/js/adapter/dal.js
+++ zygote-browser/zygote-dashboard/js/adapter/dal.js
@@ -3,6 +3,19 @@
 //dal.getWidgets(panel-name)
 //etc etc. whatever interface is suitable 
 
-//this maintains the JSON DS
-//dal.addPanel		//or delete
-//dal.addWidget 	//or delete
+/***************API*******************/
+//DB (dashboard)
+//	db.addPanel(panel)
+//	db.addWidget(panel_id, widget)
+//	db.deletePanel(panel)
+//	db.deleteWidget(widget)
+//	db.getJSON()
+
+//res (resource)
+//	res.addResource(panel_id, widget);
+//	res.getResource(ep); // "panel-id/widget-id"
+
+//flows (flows to execute)
+//	flows.getFlow(flow_id)
+//	flows.addFlow(flow_id, flow_struct);
+//	flows.deleteFlow(flow_id);
