e17ae27ecb85c4b7f76d9afd611644fcad590912
deepakkarki <deepak6k@gmail.com>
2015-04-06 22:41:53 +0530

    more or less final code for the UI adapter. It is subject to change as the UI components also develop

--- zygote-browser/zygote-dashboard/js/adapter/ui_adapter.js
+++ zygote-browser/zygote-dashboard/js/adapter/ui_adapter.js
@@ -3,15 +3,33 @@
 //talks to socket_handler.js and dal.js
 
 /*
+The UI API : 
+	getDefaultContainer() : returns Container
+	container.getPanels() : returns {<id> : <panel-obj>, ...}
+	panel.getWidgets()    : returns {<id> : <widget-obj>, ...}
+*/
+
 var container = getDefaultContainer();
+
 container.on("new-panel", function(panel){
-	console.log("new panel");
-	dal.addPanel(panel);
+
+	console.log("New panel has been created");
+	dal.db.addPanel(panel);
+
 	panel.on("new-widget", function(widget){
-		dal.addWidget(widget);
+		console.log("New widget created!");
+		dal.db.addWidget(panel.id, widget);
+		dal.res.addResource(panel.id, widget);
+	});
+
+	//should delete be a event on panel or on widget??
+	panel.on("delete-widget", function(widget){
+		console.log("Deleteing widget");
+		dal.db.deleteWidget(panel.id, widget.id);
 	});
 });
 
-
-
-*/
+container.on("delete-panel", function(panel){
+	console.log("Panel deleted");
+	dal.db.deletePanel(panel.id);
+});
