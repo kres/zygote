fd3ea071b9c370c785b68bbbc25a975e60584bcc
deepakkarki <deepak6k@gmail.com>
2015-04-08 18:49:18 +0530

    Rewrote the REST interface to handle read-write-config requests to dashboard widgets.

--- zygote-server/app/controllers/dashboard.js
+++ zygote-server/app/controllers/dashboard.js
@@ -3,159 +3,40 @@
 //This file should be later merged with socket_handler, but
 //There could be subtle differences that could lead to a problem. So seprate for now
 
-function read (panel, widget, args, callback) {
+var sc = require('./socket_handler.js');
 
-	if((panel in dashboard_tree) and (widget in dashboard_tree[panel])){
-		var obj = {"panel" : panel, "widget":widget, "data" : args};
-		ds.emit("read", obj, function(ret_val){
-				callback(ret_val);
-			});
-	}
-	else{
-		callback({"error" : "non existing panel/widget"});
-	}
-}
+exports.read = function (req, res){
+	//url : /dashboard/panel-1/widget-1
+	var url = req.url;
+	//['', dashboard, panel-1, widget-1]
+	var container = url.split('/')[1];
+	// [prev list] => [panel-1, widget-1] => 'p1/w1'
+	var id = url.split('/').slice(2).join('/');
 
-function write (panel, widget, args, callback) {
-
-	if((panel in dashboard_tree) and (widget in dashboard_tree[panel])){
-		var obj = {"panel" : panel, "widget":widget, "data" : args};
-		ds.emit("write", obj, function(ret_val){
-				callback(ret_val);
-			});
-	}
-	else{
-		callback({"error" : "non existing panel/widget"});
-	}
-}
-
-// Don't know if config should be allowed!
-// all should be done from the user side.....
-function config (panel, widget, args, callback) {
-
-	if((panel in dashboard_tree) and (widget in dashboard_tree[panel])){
-		var obj = {"panel" : panel, "widget":widget, "data" : args};
-		ds.emit("config", obj, function(ret_val){
-				callback(ret_val);
-			});
-	}
-	else{
-		callback({"error" : "non existing panel/widget"});
-	}
-}
-
-exports.read = read;
-exports.write = write;
-exports.config = config;
-
-/******************************************************************/
-
-//The following two functions are the result of user creating/deleting widgets on dashboard
-
-function create (panel, widget, args) {
-	// manipulate the dashboard_tree
-	if(!panel || !widget){
-		return {"error" : "data in wrong format"};
-	}
-
-	if(panel in dashboard_tree){
-		dashboard_tree[panel][widget] = args;
-		return {"status" : "ok"};
-	}else{
-		return {"error" : "panel does not exist"};
-	}
-}
-
-function del (panel, widget) {
-	// manipulate the dashboard_tree
-	if(!panel || !widget){
-		return {"error" : "data in wrong format"};
-	}
-
-	if(panel in dashboard_tree and widget in dashboard_tree[panel]){
-		delete dashboard_tree[panel][widget];
-		return {"status" : "ok"};
-	}else{
-		return {"error" : "panel/widget does not exist"};
-	}
-}
-
-// following structure is updated by create and delete
-// For internal use only
-// Should expose functions to get the reqd values to display in flowboard
-		//function get_panels()
-		//function get_widgets("panel-id");
-
-var dashboard_tree = {}
-/*
-{
-	"panel-id" : {
-		"widget-id" : {
-			"type" : "heater"
-			"other setting" : "setting value"
-			...
-		},
-
-		"widget-id" : {
-			...
-		}
-	},
-
-	"panel-id" : {
-		....
-	}
-	...
-}
-*/
-
-/*
-	Return the list of panels present in the dashboard
-	["p1", "p2", "p3", ...]
-*/
-function get_panels(){
-	return Object.keys(dashboard_tree);
-}
-
-/*
-	Returns the json obj of widgets (& its info)
-
-	{
-		"w-id" : {widget info},
-		"w-id" : {widget info},
-		...
-	}
-*/
-function get_widgets (panel_id) {
-	if(panel_id in dashboard_tree){
-		return dashboard_tree[panel_id];
-	}
-	return {"error" : "panel not found"}
-}
-
-exports.get_panels = get_panels;
-exports.get_widgets = get_widgets;
-
-/*****************************************************************************/
-
-//The main socket handler
-var ds;
-exports.startSocket = function(server){
-	//create the web-socket manager
-	var io = require('socket.io')(server);
-	var dsr = io.of('/dashboard');
-
-	dsr.on("connection", function(socket){
-		console.log("Connected to dashboard!!");
-		ds = socket;
-
-		socket.on("create", function(data, callback){
-			var res = create(data['panel'], data['widget'], data['widget-info']);
-			callback(res);
-		});
-
-		socket.on("delete", function(data, callback){
-			var res = del(data['panel'], data['widget']);
-			callback(res);
-		});
+	sc.read(container, id, req.query, function(data){
+		res.json(data);
 	});
-}
+};
+
+exports.write = function (req, res){
+	var url = req.url;
+
+	var container = url.split('/')[1];
+	var id = url.split('/').slice(2).join('/');
+
+	sc.write(container, id, req.body, function(data){
+		res.json(data);
+	});
+};
+
+exports.config = function (req, res){
+	var url = req.url;
+
+	var container = url.split('/')[1];
+	var id = url.split('/').slice(2).join('/');
+
+	sc.config(container, id, req.body, function(data){
+		res.json(data);
+	});
+};
+
