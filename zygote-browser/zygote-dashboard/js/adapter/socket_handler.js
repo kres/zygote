f7cb436a0585e973dcf08df5091ba303d358af58
deepakkarki <deepak6k@gmail.com>
2015-04-11 20:23:20 +0530

    Socket should automatically figure out where to connect

--- zygote-browser/zygote-dashboard/js/adapter/socket_handler.js
+++ zygote-browser/zygote-dashboard/js/adapter/socket_handler.js
@@ -4,7 +4,9 @@
 //socket handler namespace 
 var sh = {};
 
-var socket = io('/dashboard');
+//io() should automatically figure out where to connect
+//I don't want to hardcode for localhost
+var socket = io();
 socket.on('connect', function(){
 	console.log("Connected to the Server");
 	socket.emit('url', {'url' : 'dashboard'});
