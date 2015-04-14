3894e2a3eccfbd6799eb889230c599ed215d7577
deepakkarki <deepak6k@gmail.com>
2015-04-15 00:05:34 +0530

    modified the adapter to match event names

--- zygote-browser/zygote-dashboard/js/adapter/ui_adapter.js
+++ zygote-browser/zygote-dashboard/js/adapter/ui_adapter.js
@@ -11,26 +11,26 @@ The UI API :
 
 var container = getDefaultContainer();
 
-container.on("new-panel", function(panel){
+container.on("panel-added", function(panel){
 
 	console.log("New panel has been created");
 	dal.db.addPanel(panel);
 
-	panel.on("new-widget", function(widget){
+	panel.on("widget-added", function(widget){
 		console.log("New widget created!");
 		dal.db.addWidget(panel.id, widget);
 		dal.res.addResource(panel.id + "/" + widget.id);
 	});
 
 	//should delete be a event on panel or on widget??
-	panel.on("delete-widget", function(widget){
+	panel.on("widget-removed", function(widget){
 		console.log("Deleteing widget");
 		dal.db.deleteWidget(panel.id, widget);
 		dal.res.deleteResource(panel.id + "/" + widget.id);
 	});
 });
 
-container.on("delete-panel", function(panel){
+container.on("panel-removed", function(panel){
 	console.log("Panel deleted");
 	dal.db.deletePanel(panel);
 });
