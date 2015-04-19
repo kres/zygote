3950c3064649b1bd1990076113d27755fb6efddc
deepakkarki <deepak6k@gmail.com>
2015-04-19 20:46:13 +0530

    Added temp sensor arduino sketch

---
+++ remote-nodes/pc/wifi-lm35.ino
@@ -0,0 +1,19 @@
+//Temp sensor sketch that works with lm35@A0
+float temp;
+int tempPin = 0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  if(Serial.available() > 0)
+  {
+    //clear the buffer - indicates request
+    Serial.read();
+    //read adc val
+    temp = analogRead(tempPin);
+    temp = temp * 0.48828125;
+    Serial.print(temp);
+  }
+}
